261e8ed19920c46da95c6faeeda6960afafb1988
Armando Almanza <armando.s.almanza@gmail.com>
2023-01-20 20:56:20 -0600

    its better?

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,38 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+    const links = [
+        {
+            to: "/",
+            name: "Home",
+        },
+        {
+            to: "/profile",
+            name: "Profile",
+        },
+        {
+            to: "/",
+            name: "Posts",
+        },
+        {
+            to: "/",
+            name: "Friends",
+        },
+        {
+            to: "/",
+            name: "Trending",
+        },
+    ];
+
     return (
         <nav className="navbar">
-            <Link to="/" className="link">
-                Home
-            </Link>
-
-            <Link to="/profile" className="link">
-                Profile
-            </Link>
+            {links.map((anchor, i) => {
+                return (
+                    <Link to={anchor.to} className="link" key={i}>
+                        {anchor.name}
+                    </Link>
+                );
+            })}
         </nav>
     );
 };
