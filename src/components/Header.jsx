dd14c6967c1c142c6e4570ec7049c52835d0bc80
Armando Almanza <armando.s.almanza@gmail.com>
2023-01-20 22:04:45 -0600

    tbh im bored jajaja

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
-import Navbar from './Navbar'
-import avatar from "../assets/avatar.svg"
+import Navbar from "./Navbar";
+import avatar from "../assets/avatar.svg";
+
+const showMessage = () => {
+    alert("you are logged");
+};
 
 const Header = () => {
-  return (
-	<header className='site__header'>
-		<div>
-			<h1 className='logo'>MALA <span>Social media</span></h1>
-		</div>
-		<Navbar />
-		<div className='profile__header'>
-			<img className='avatar__header' src={avatar} alt="Avatar picture" />
-			<h1>Arandano Depresivo</h1>
-		</div>
-	</header>
-  )
-}
+    return (
+        <header className="site__header">
+            <div>
+                <h1 className="logo">
+                    MALA <span>Social media</span>
+                </h1>
+            </div>
+            <Navbar />
+            <div className="profile__header" onClick={showMessage}>
+                <img
+                    className="avatar__header"
+                    src={avatar}
+                    alt="Avatar picture"
+                />
+                <h1>Arandano Depresivo</h1>
+            </div>
+        </header>
+    );
+};
 
-export default Header
+export default Header;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,17 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    const links = [
-        {
-            to: "/",
-            name: "Home",
-        },
-        {
-            to: "/profile",
-            name: "Profile",
-        },
-        {
-            to: "/",
-            name: "Posts",
-        },
-        {
-            to: "/",
-            name: "Friends",
-        },
-        {
-            to: "/",
-            name: "Trending",
-        },
-    ];
+    const links = ["profile", "posts", "friends", "trending"];
 
     return (
         <nav className="navbar">
+            <Link to={"/"} className="link">
+                Home
+            </Link>
             {links.map((anchor, i) => {
                 return (
-                    <Link to={anchor.to} className="link" key={i}>
-                        {anchor.name}
+                    <Link to={"/" + anchor} className="link" key={i}>
+                        {anchor.charAt(0).toUpperCase() + anchor.slice(1)}
                     </Link>
                 );
             })}
