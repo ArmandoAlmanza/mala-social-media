b0883a68b9566d152ac248829a68265b08c783d0
Armando Almanza <armando.s.almanza@gmail.com>
2023-01-26 16:27:27 -0600

    add loader

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Loader from "./Loader";
 import avatar from "../assets/avatar.svg";
 
 const getData = async () => {
@@ -9,10 +10,15 @@ const getData = async () => {
 
 const Posts = () => {
     const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
     useEffect(() => {
-        getData().then((data) => {
-            setPosts(data);
-        });
+        setLoading(true);
+        setTimeout(() => {
+            getData().then((data) => {
+                setPosts(data);
+                setLoading(false);
+            });
+        }, 5000);
     }, []);
 
     const names = [
@@ -25,26 +31,32 @@ const Posts = () => {
 
     return (
         <div>
-            <div className="post__container">
-                {posts.slice(0, 20).map((post, i) => (
-                    <div key={i} className="post">
-                        <h1>{post.title}</h1>
-                        <p>{post.body}</p>
-                        <article className="post__info">
-                            <img src={avatar} alt="avatar image" />
-                            <p>
-                                Writen by{" "}
-                                {
-                                    names[
-                                        Math.floor(Math.random() * names.length)
-                                    ]
-                                }{" "}
-                            </p>
-                            <LikeButton id={i} />
-                        </article>
-                    </div>
-                ))}
-            </div>
+            {loading ? (
+                <Loader />
+            ) : (
+                <div className="post__container">
+                    {posts.slice(0, 20).map((post, i) => (
+                        <div key={i} className="post">
+                            <h1>{post.title}</h1>
+                            <p>{post.body}</p>
+                            <article className="post__info">
+                                <img src={avatar} alt="avatar image" />
+                                <p>
+                                    Writen by{" "}
+                                    {
+                                        names[
+                                            Math.floor(
+                                                Math.random() * names.length
+                                            )
+                                        ]
+                                    }{" "}
+                                </p>
+                                <LikeButton id={i} />
+                            </article>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
