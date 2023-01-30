2c0c07881ebee9e81d95b032fda7f7c88834040b
Armando Almanza <armando.s.almanza@gmail.com>
2023-01-30 15:32:27 -0600

    validate user creation

--- Server/src/main/java/com/mala/user/UserController.java
+++ Server/src/main/java/com/mala/user/UserController.java
@@ -2,11 +2,13 @@ package com.mala.user;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import lombok.AllArgsConstructor;
 
@@ -14,9 +16,11 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @RequestMapping("/api/user")
 public class UserController {
-	
+
 	private final UserRepository userRepository;
-	record newUserRequest (String name, String email, String password, String imagePath){}
+
+	record newUserRequest(String name, String email, String password, String imagePath) {
+	}
 
 	@GetMapping
 	public List<User> getUsers() {
@@ -24,12 +28,21 @@ public class UserController {
 	}
 
 	@PostMapping
-	public User newUser(@RequestBody newUserRequest req){
+	public User newUser(@RequestBody newUserRequest req) {
 		User user = new User();
+		if (req.name() == null || req.name() == "") {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Need to add a user name");
+		}
+
+		if (req.imagePath() == null || req.imagePath() == "") {
+			user.setImagePath("https://avatars.dicebear.com/api/adventurer/your-custom-seed.svg");
+		} else {
+			user.setImagePath(req.imagePath());
+		}
+		
 		user.setName(req.name());
 		user.setEmail(req.email());
 		user.setPassword(req.password());
-		user.setImagePath(req.imagePath());
 		userRepository.save(user);
 		return user;
 	}
