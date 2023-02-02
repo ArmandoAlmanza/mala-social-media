7eab88175323f6605fe22ca9fe3fce999c421227
Armando Almanza <armando.s.almanza@gmail.com>
2023-02-01 20:55:04 -0600

    add the post entity (not complete)

--- Server/src/main/java/com/mala/post/Post.java
+++ Server/src/main/java/com/mala/post/Post.java
@@ -1,6 +1,9 @@
 package com.mala.post;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,6 +15,9 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "post")
 public class Post {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
 	private String title;
 	private String body;
 	private String user;
---
+++ Server/src/main/java/com/mala/post/PostController.java
@@ -0,0 +1,44 @@
+package com.mala.post;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/post")
+public class PostController {
+	private final PostRepository postRepository;
+
+	record newPostRequest(String title, String body, String user, boolean like) {
+	}
+
+	@GetMapping()
+	public List<Post> getPosts() {
+		return postRepository.findAll();
+	}
+
+	@PostMapping()
+	public Post addPost(@RequestBody newPostRequest req) {
+		Post post = new Post();
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		LocalDateTime now = LocalDateTime.now();
+		post.setTitle(req.title());
+		post.setBody(req.body());
+		post.setUser(req.user());
+		post.setLike(req.like());
+		post.setCreationDate(dtf.format(now));
+
+		return post;
+	}
+
+}
