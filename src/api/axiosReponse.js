67b57da65be77b088a6bf27ddf9cccf14cca6d46
Armando Almanza <armando.s.almanza@gmail.com>
2023-02-13 12:29:53 -0600

    add the "update" parameter instead of create-drop

--- src/api/axiosReponse.js
+++ src/api/axiosReponse.js
@@ -6,3 +6,6 @@ export const getPost = async (id) => axios.get("/post/" + id);
 
 // New post
 export const newPost = async (user) => axios.post("/post", user);
+
+//Update like post
+export const updateLike = async (id) => axios.put("/post/" + id);
--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import { getPost, updateLike } from "../api/axiosReponse";
 const LikeButton = ({ id }) => {
     const [like, setLike] = useState(false);
+    const [post, setPosts] = useState({});
+
+    useEffect(() => {
+        getPost(id).then((data) => {
+            setPosts(data.data);
+            setLike(data.data.liked);
+        });
+    }, []);
     return (
         <button
             className="btn"
             onClick={() => {
-                setLike(!like);
+                setLike(!post.liked);
+                updateLike(id);
             }}
         >
             {like ? "❤" : "🤍"}
